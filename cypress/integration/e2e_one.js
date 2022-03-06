2e153550045e9e131b3b4c45c58436d55169b5da
Vadym Karpych <vadimclz@gmail.com>
2022-03-06 00:13:46 -0700

    init

---
+++ cypress/integration/e2e_one.js
@@ -0,0 +1,37 @@
+describe("E2E test", () => {
+
+    before(() => {
+        cy.visit('http://qalab.pl.tivixlabs.com/');
+    });
+
+    it('A user should be able to search for a car in specific country/city', () => {
+
+        cy.get('#country').select('Poland');
+        cy.get('#city').select('Cracow');
+        cy.get('#model').type('Toyota');
+
+        cy.get('#pickup').type('2022-03-09').should('have.value', '2022-03-09');
+        cy.get('#dropoff').type('2022-03-15').should('have.value', '2022-03-15');
+
+        cy.get('button[type="submit"]').click();
+
+        cy.get('tbody')
+            .find('tr')
+            .contains('Toyota')
+            .parents('tr')
+            .find('a:contains("Rent")')
+            .click()
+
+        cy.get('.card-header').contains('Toyota')
+        cy.get('.card-body > :nth-child(3)').contains('Location: Poland, Cracow')
+        cy.get('a:contains("Rent!")').click()
+        cy.get('.col-md-4 > :nth-child(6)').should('have.text', ' Pickup date: 2022-03-09')
+        cy.get('.col-md-4 > :nth-child(7)').should('have.text', ' Dropoff date: 2022-03-15')
+
+        cy.get('#name').type('Michael')
+        cy.get('#last_name').type('Jackson')
+        cy.get('#card_number').type('4988438843884305')
+        cy.get('#email').type('test@test.com')
+        cy.get('button:contains("Rent")').click()
+    });
+});
---
+++ cypress/integration/e2e_two.js
@@ -0,0 +1,19 @@
+describe("My first testSuite", () => {
+
+    beforeEach(() => {
+        cy.visit("http://qalab.pl.tivixlabs.com/");
+    });
+
+    it('My first testCase', () => {
+
+        cy.get('#country').select('Poland');
+        cy.get('#city').select('Cracow');
+        cy.get('#model').type('Toyota');
+
+        cy.get('#pickup').type('2022-03-03').should('have.value', '2022-03-03');
+        cy.get('#dropoff').type('2022-02-28').should('have.value', '2022-02-28');
+
+        cy.get('button[type="submit"]').click();
+        cy.get('h3').should('have.text', 'Please enter a valid date!');
+    });
+});
